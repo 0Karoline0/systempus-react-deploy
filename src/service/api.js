97f2345082a3519463287bcf303566662f9f139b
0Karoline0 <karolinevmfaria@gmail.com>
2025-01-28 20:03:39 -0300

    :wrench: feat: Ajuste url

--- src/service/api.js
+++ src/service/api.js
@@ -1,6 +1,6 @@
 
 // const baseUrl = 'http://localhost:8080/api/v1/';
-const url = 'https://systempus.onrender.com/api/v1/';
+const baseUrl = 'https://systempus.onrender.com/api/v1/';
 
 export const fetchEntidades = async (endpoint) => {
     try {
